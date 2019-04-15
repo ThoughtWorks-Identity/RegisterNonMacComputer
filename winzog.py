110852f62cc2c64c4d4a56b538d465821bab46cb
Steve Quirke <squirke@thoughtworks.com>
2019-04-15 17:10:40 +0100

    added webbrowser library to launch Rolzog

--- winzog.py
+++ winzog.py
@@ -3,9 +3,14 @@ import wmi
 import json
 import getmac
 import requests
+import webbrowser
 
 for config in wmi.WMI().Win32_ComputerSystemProduct():
     data = {'serial_number': config.IdentifyingNumber, 'laptop_vendor': config.Vendor, 'laptop_type': config.Name}
 
 print(getmac.get_mac_address())
 print(data)
+
+url = "https://topsecret.url"
+
+webbrowser.open_new_tab(url)
