31b285b0125b11c23628c7e78808853040138dc6
sethu <sethur@thoughtworks.com>
2019-05-01 19:06:41 -0700

    Sethu | cleaned up some code

--- src/winzog.py
+++ src/winzog.py
@@ -1,12 +1,11 @@
 import wmi
 # wmi also requires a pip install pypiwin32
-import json
 import getmac
-import requests
 import webbrowser
 import os
-from urllib.parse import quote
 import sys
+import urllib.parse
+
 
 # This should suffice for now. When we have more variables consider using the
 # ConfigParser module
@@ -17,35 +16,26 @@ def getBaseUrl():
         # we are running in a normal Python environment
         bundle_dir = os.path.dirname(os.path.abspath(__file__))
 
-    with open(bundle_dir+'/registrationWebAppUrl.txt', 'r') as registrationWebAppUrlFile:
+    with open(bundle_dir + '/registrationWebAppUrl.txt', 'r') as registrationWebAppUrlFile:
         base_url = registrationWebAppUrlFile.read()
 
     print(base_url)
-    return base_url
-    
+    return base_url.strip()
+
+
 base_url = getBaseUrl()
+config = wmi.WMI().Win32_ComputerSystemProduct()[0]
 
-for config in wmi.WMI().Win32_ComputerSystemProduct():
-    data = {'serial_number': config.IdentifyingNumber, 'laptop_vendor': config.Vendor, 'laptop_type': config.Name}
+data = {
+    'serial_number': config.IdentifyingNumber,
+    'laptop_vendor': config.Vendor,
+    'laptop_type': config.Name,
+    'mac': getmac.get_mac_address()
+}
 
-    try:
-        serial = config.IdentifyingNumber
-        model = config.Vendor + " " + config.Name
-    except:
-        print("Something went wrong!")
-
-for k,v in data.items():
-    data[k] = quote(v)
-
-mac = getmac.get_mac_address()
-
-print(getmac.get_mac_address())
 print(data)
 
-print(serial)
-print(model)
-
-winzog_url = base_url + "serial={0}&model={1}".format(serial, model)
+winzog_url = base_url + urllib.parse.urlencode(data)
 
 print(winzog_url)
 webbrowser.open_new_tab(winzog_url)
