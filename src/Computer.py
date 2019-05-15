4676685c3ec3a33922d8fbacff903f36a81c35fe
sethu <sethur@thoughtworks.com>
2019-05-15 16:23:52 +1000

    Sethu | moving to using the qmi library instead of executing commands

--- src/Computer.py
+++ src/Computer.py
@@ -1,34 +1,31 @@
 import wmi
-# wmi also requires a pip install pypiwin32
 import getmac
-from pathlib import Path
 import platform
-import re
-from subprocess import check_output, CalledProcessError, DEVNULL
 
 
-def is_service_running(sc_query_output):
-    return bool(re.search(r'\s*?STATE\s*?:\s*?4\s*?RUNNING', sc_query_output))
-
-
-def check_service(query):
-    try:
-        sc_query_output = check_output(query, stdin=DEVNULL).decode('utf-8')
+def check_service(service_name):
+    c = wmi.WMI()
+    service_list = c.Win32_Service(Name=service_name)
+    if len(service_list) > 0:
+        service = service_list[0]
         return {
             "is_installed": "true",
-            "is_running": "true" if is_service_running(sc_query_output) else "false"
+            "is_running": "true" if service.state == 'Running' else "false"
         }
-    except CalledProcessError:
+    else:
         return {
             "is_installed": "false",
             "is_running": "false"
         }
 
+
 def get_sophos_status():
-    return check_service('sc query SAVService')
+    return check_service('SAVService')
+
 
 def get_windows_os_version():
     return platform.platform()
+
 
 def get_device_data():
     config = wmi.WMI().Win32_ComputerSystemProduct()[0]
@@ -40,16 +37,16 @@ def get_device_data():
         'wifi_mac': getmac.get_mac_address()
     }
 
-def get_bit_locker_status(): 
-    return check_service('sc query BDESVC')
-    
+
+def get_bit_locker_status():
+    return check_service('BDESVC')
+
 
 def get_data():
-    
+
     return {
         "device_data": get_device_data(),
         "sophos": get_sophos_status(),
         "bitlocker": get_bit_locker_status(),
         "windows_version": get_windows_os_version(),
     }
-  
