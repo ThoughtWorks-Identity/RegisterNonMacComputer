8d02887268b6c7dc04f0ecefa3b13b3d8ca54dc2
sethu <sethur@thoughtworks.com>
2019-05-17 09:46:52 +1000

    Sethu - allowing the exe to show a console so we can debug the problem

--- makeInstaller.sh
+++ makeInstaller.sh
@@ -7,6 +7,4 @@ sumo_url_cleaned="${sumo_url%\"}"
 sumo_url_cleaned="${sumo_url_cleaned#\"}"
 echo registration_url=${reg_url_cleaned} > configuration.properties
 echo sumo_logic_url=${sumo_url_cleaned} >> configuration.properties
-python --version
-#python -m pyinstaller --onefile --add-data "configuration.properties:." -n RegisterLinuxComputer src/RegisterYourComputer.py
 pyinstaller --onefile --add-data "configuration.properties:." -n RegisterLinuxComputer src/RegisterYourComputer.py
