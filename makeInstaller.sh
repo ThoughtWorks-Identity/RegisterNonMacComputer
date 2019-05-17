fdd2da64db0700414702b567d588a843cb7d5dca
sethu <sethur@thoughtworks.com>
2019-05-17 11:27:09 +1000

    Sethu | adding some debug for the build

--- makeInstaller.sh
+++ makeInstaller.sh
@@ -7,4 +7,6 @@ sumo_url_cleaned="${sumo_url%\"}"
 sumo_url_cleaned="${sumo_url_cleaned#\"}"
 echo registration_url=${reg_url_cleaned} > configuration.properties
 echo sumo_logic_url=${sumo_url_cleaned} >> configuration.properties
+echo "version of python"
+python --version
 pyinstaller --onefile --paths="src" --hidden-import "src/LinuxComputer" --add-data "configuration.properties:." -n RegisterLinuxComputer src/RegisterYourComputer.py
