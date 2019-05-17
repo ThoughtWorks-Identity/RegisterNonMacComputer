731a3b68c511111b12461f58f58685b388dd3dd2
sethu <sethur@thoughtworks.com>
2019-05-17 10:07:23 +1000

    Sethu fixed the hidden import problem

--- makeInstaller.sh
+++ makeInstaller.sh
@@ -7,4 +7,4 @@ sumo_url_cleaned="${sumo_url%\"}"
 sumo_url_cleaned="${sumo_url_cleaned#\"}"
 echo registration_url=${reg_url_cleaned} > configuration.properties
 echo sumo_logic_url=${sumo_url_cleaned} >> configuration.properties
-pyinstaller --onefile --add-data "configuration.properties:." -n RegisterLinuxComputer src/RegisterYourComputer.py
+pyinstaller --onefile --paths="src" --hidden-import "src/LinuxComputer" --add-data "configuration.properties:." -n RegisterLinuxComputer src/RegisterYourComputer.py
