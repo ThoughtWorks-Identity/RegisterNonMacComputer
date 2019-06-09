1bd737ff9b0e14ebdd32a221cd0a6b25db85c4d0
meghah <meghahavaragi@gmail.com>
2019-06-09 16:43:35 +0530

    copy nonmac laptop reg pkgs to s3

--- buildAndDeploy/deploy.sh
+++ buildAndDeploy/deploy.sh
@@ -4,4 +4,5 @@ set -e
 bucket=$1
 filename=$2
 
-aws s3 sync ${filename} s3://${bucket}/laptop-registration/${filename} --delete
+aws s3 cp ${filename} s3://${bucket}/laptop-registration/RegisterLinuxComputer
+aws s3 cp ${filename} s3://${bucket}/laptop-registration/RegisterYourComputer.exe
