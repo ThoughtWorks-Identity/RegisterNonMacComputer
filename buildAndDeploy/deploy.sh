82c1c7bcd25f02b320fce1350cd63dfb3f57d422
meghah <meghahavaragi@gmail.com>
2019-06-09 17:13:12 +0530

    fix synax error

--- buildAndDeploy/deploy.sh
+++ buildAndDeploy/deploy.sh
@@ -4,7 +4,6 @@ set -e
 bucket=$1
 win_filename=$2
 lin_filename=$3
-fil
 
 aws s3 cp ${win_filename} s3://${bucket}/laptop-registration/RegisterYourComputer.exe
 aws s3 cp ${lin_filename} s3://${bucket}/laptop-registration/RegisterLinuxComputer
