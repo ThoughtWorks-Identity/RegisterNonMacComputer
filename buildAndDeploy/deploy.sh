d780e107cfa915340f57defb7ec83f127e3dab43
meghah <meghahavaragi@gmail.com>
2019-06-09 17:02:48 +0530

    copy win and linux pkgs to s3

--- buildAndDeploy/deploy.sh
+++ buildAndDeploy/deploy.sh
@@ -2,7 +2,11 @@
 set -e
 
 bucket=$1
-filename=$2
+win_filename=$2
+lin_filename=$3
+fil
 
-aws s3 cp ${filename} s3://${bucket}/laptop-registration/RegisterLinuxComputer
-aws s3 cp ${filename} s3://${bucket}/laptop-registration/RegisterYourComputer.exe
+aws s3 cp ${win_filename} s3://${bucket}/laptop-registration/RegisterYourComputer.exe
+aws s3 cp ${lin_filename} s3://${bucket}/laptop-registration/RegisterLinuxComputer
+
+
