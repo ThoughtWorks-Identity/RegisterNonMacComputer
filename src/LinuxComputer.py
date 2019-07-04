48ae4f29b038a3e0c88a761f83b0b85527264432
sethu <sethur@thoughtworks.com>
2019-07-05 09:11:54 +1000

    Sethu | gettting system product name if system version returns empty

--- src/LinuxComputer.py
+++ src/LinuxComputer.py
@@ -4,18 +4,29 @@ import platform
 
 
 def get_from_dmidecode(key):
-    command = ['sudo','dmidecode', '-s', key]
+    command = ['sudo', 'dmidecode', '-s', key]
     output = subprocess.check_output(command).decode('UTF-8')
     return output.strip()
 
+
+def get_model():
+    model = get_from_dmidecode('system-version')
+    if not model.strip():
+        model = get_from_dmidecode('system-product-name')
+    if not model.strip():
+        raise Exception("Unable to find model for this computer. Please report the issue to MDM - Techops")
+    return model.strip()
+
+
 def get_device_data():
-     return {
+    return {
         'serial': get_from_dmidecode('system-serial-number'),
         'non_mac': "true",
         'manufacturer': get_from_dmidecode('system-manufacturer'),
-        'model': get_from_dmidecode('system-version'),
+        'model': get_model(),
         'wifi_mac': getmac.get_mac_address()
     }
+
 
 def get_data():
     return {
